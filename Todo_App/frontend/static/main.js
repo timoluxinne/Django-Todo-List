44b2aa506a54b7223f22e5c87c05060fa9a48290
timoh_ish <timredblack8@gmail.com>
2020-11-18 05:05:21 +0300

    Fetch data

--- Todo_App/frontend/static/main.js
+++ Todo_App/frontend/static/main.js
@@ -1,7 +1,16 @@
 console.log('====================================');
 console.log('Youh');
 console.log('====================================');
+BuildList()
 
 function BuildList(){
-    
+    console.log('Youh');
+    var wrapper = document.getElementById('list-wrapper')
+    var url = 'http://localhost:8000/api/task-list/'
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log('Data:',data)
+    })
 }
