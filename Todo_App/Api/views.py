f2c884329f025b74bf2ab4290c001fe14634669a
timoh_ish <timredblack8@gmail.com>
2020-11-18 04:08:26 +0300

    Create apiOverview

--- Todo_App/Api/views.py
+++ Todo_App/Api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'list view': '/task-list/',
+        'Create view': '/task-new/',
+        'Detail view': '/task-detail/<str:pk>/',
+        'Update view': '/task-update/<str:pk>/',
+        'Delete view': '/task-delete/<str:pk>/',
+    }
+
+    return Response(api_urls)
