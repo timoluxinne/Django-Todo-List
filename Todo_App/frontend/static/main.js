b3397d855fed48b75cbd0638777f1054d72894d4
timoh_ish <timredblack8@gmail.com>
2020-11-18 05:29:06 +0300

    Update list wrapper

--- Todo_App/frontend/static/main.js
+++ Todo_App/frontend/static/main.js
@@ -12,5 +12,20 @@ function BuildList(){
     .then(res => res.json())
     .then(data => {
         console.log('Data:',data)
+        
+        var list = data
+        for(let i=0; i< list.length; i++){
+            // console.log(i);
+            var item = `
+                <div id='task-wrapper' class='d-flex justify-content-between'>
+                    <span>${list[i].title}</span>
+                    <div>
+                        <button class='btn btn-outline-info'>Edit</button>
+                        <button class='btn btn-outline-danger'>Del</button>
+                    </div>
+                </div>
+            `
+            wrapper.innerHTML += item
+        }
     })
 }
