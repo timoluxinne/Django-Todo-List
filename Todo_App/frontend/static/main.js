7af0aaf4dbc6891a6723345dc11e945f55e370ce
timoh_ish <timredblack8@gmail.com>
2020-11-18 18:32:42 +0300

    Add Strike Functionality

--- Todo_App/frontend/static/main.js
+++ Todo_App/frontend/static/main.js
@@ -33,12 +33,18 @@ function BuildList(){
         
         var list = data
         for(let i=0; i< list.length; i++){
+            var title = `<span class='title'>${list[i].title}</span>`
+
+            if (list[i].complete == true){
+                title = `<strike class='title text-black-50'>${list[i].title}</strike>`
+            }
+
             // console.log(i);
             var item = `
                 <div id="list-wrapper" class="container bg-light p-0 m-0">
                     <div class="d-flex justify-content-between border-bottom px-3 py-2">
                         <div class="pt-2">
-                            <span>${list[i].title}</span>
+                            ${title}
                         </div>
                         <div>
                             <button class="btn btn-outline-info edit">Edit</button>
@@ -53,7 +59,7 @@ function BuildList(){
         for(let i=0; i< list.length; i++){
             var editBtn = document.getElementsByClassName('edit')[i]
             var deleteBtn = document.getElementsByClassName('delete')[i]
-
+            var title = document.getElementsByClassName('title')[i]
 
             editBtn.addEventListener('click', function(item){
                 return function(){
@@ -64,6 +70,12 @@ function BuildList(){
             deleteBtn.addEventListener('click', function(item){
                 return function(){
                     deleteItem(item)
+                };
+            }(list[i]));
+
+            title.addEventListener('click', function(item){
+                return function(){
+                    strokeUnstroke(item)
                 };
             }(list[i]));
         }
@@ -105,7 +117,6 @@ function editItem(item){
 }
 
 function deleteItem(item){
-    console.log('Item:', item);
 
     fetch(`http://localhost:8000/api/task-delete/${item.id}/`, {
         method: 'DELETE',
@@ -117,5 +128,23 @@ function deleteItem(item){
     })
     .then(data => {
         BuildList()
+    })
+}
+
+function strokeUnstroke(item){
+    console.log('Stroke:', item);
+    item.complete = !item.complete
+
+    fetch(`http://localhost:8000/api/task-update/${item.id}/`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify({'title': item.title, 'complete': item.complete})
+    })
+    .then(data => {
+        BuildList()
+        location.reload()
     })
 }
